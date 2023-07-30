757ce3cc7634e5518b6487a11e431f7a6969f13b
nassib <younes.nassib@gmail.com>
2023-07-30 13:00:44 +0100

    last commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,8 +7,7 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        super.onCreate(savedInstanceState)
-        Log.d("jdjud,""jfjff")
+
 
     }
 }
