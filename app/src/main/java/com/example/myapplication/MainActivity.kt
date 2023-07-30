3fbb49b5fcada70294ee7cc9ef3829a20a6906bb
nassib <younes.nassib@gmail.com>
2023-07-30 12:58:11 +0100

    stash

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,17 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         super.onCreate(savedInstanceState)
-        super.onCreate(savedInstanceState)
-        super.onCreate(savedInstanceState)
-        super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main)
-
-        Log.d("hldhd","jdidid")
-
 
     }
 }
